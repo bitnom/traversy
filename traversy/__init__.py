64380ff242b8a870d4f6f3d5f603b554474ae4b7
TensorTom <14287229+TensorTom@users.noreply.github.com>
2020-11-21 22:53:21 -0500

    Offload non-traversal defs to datafunc package. Add type hinting.

--- traversy/__init__.py
+++ traversy/__init__.py
@@ -1,6 +1,8 @@
-from mo_dots import Data, FlatList
+from mo_dots import Data, FlatList, DataObject
 from dotty_dict import Dotty
 import copy
+from datafunc import listlike, dictlike
+from typing import Dict, List, Literal, Tuple, Union, Any, Callable, Set, Optional, AnyStr
 
 
 def func(var):
@@ -11,9 +13,10 @@ def return_all(*args, **kwargs):
 	return True
 
 
-def traverse(data, filter_func=return_all, node_path=None,
-             parent_node=None, output_formatter=func,
-             val_transformer=func, **kwargs):
+def traverse(data: Union[Dict, Data, DataObject, Dotty, List, FlatList],
+             filter_func: Callable = return_all, node_path: Optional[List] = None,
+             parent_node: Optional[Any] = None, output_formatter: Callable = func,
+             val_transformer: Callable = func, **kwargs):
 	"""
 	Traverse deep data structures such as dict, mo-dots, and dotty_dict. Supports
 	nested lists & data.
@@ -24,54 +27,28 @@ def traverse(data, filter_func=return_all, node_path=None,
 		:param output_formatter: Function applied to entire return dict.
 		:param val_transformer: Apply func to yielded return val.
 		:param kwargs: All extra keyword args are sent to the filter_func()
-		:return:
+		:return: Yields an iterator result-set for each traversable key/value pair
+					and/or list item to an infinite depth.
 	"""
 	node_path = [] if node_path is None else node_path
-	path_str = '.'.join([str(n) for n in node_path])
-	if isinstance(data, (dict, Data, Dotty)):
+	path: AnyStr = '.'.join([str(n) for n in node_path])
+	if dictlike(data):
 		for x in data.keys():
-			local_path = node_path[:]
+			local_path: List = node_path[:]
 			local_path.append(x)
 			yield from traverse(data[x], filter_func=filter_func, node_path=local_path,
 			                    parent_node=data, output_formatter=output_formatter,
 			                    val_transformer=val_transformer, **kwargs)
-	elif isinstance(data, (list, FlatList)):
+	elif listlike(data):
 		for x, y in enumerate(data):
-			local_path = node_path[:]
+			local_path: List = node_path[:]
 			local_path.append(x)
 			yield from traverse(data[x], filter_func=filter_func, node_path=local_path,
 			                    parent_node=data, output_formatter=output_formatter,
 			                    val_transformer=val_transformer, **kwargs)
 	elif filter_func(node_path[-1:], data, node_path, **kwargs):
-		key = node_path[-1:][0] if len(node_path[-1:]) > 0 else ''
+		key: AnyStr = node_path[-1:][0] if len(node_path[-1:]) > 0 else ''
 		yield output_formatter({'key': key, 'value': val_transformer(data), 'node_path': node_path,
-		                        'path_str': path_str, 'filter_func': filter_func.__name__,
+		                        'path': path, 'filter_func': filter_func.__name__,
 		                        'filter_args': (data, kwargs), 'parent_node': parent_node,
 		                        'output_formatter': output_formatter.__name__})
-
-
-def duplicate(data):
-	"""
-	Convenience method for copy.deepcopy()
-	:param data: Any dict, mo-dots, or dotty object.
-	:return: A deep copy of the data.
-	"""
-	return copy.deepcopy(data)
-
-
-def add_sibling(data, node_path, new_key, new_data, _i=0):
-	"""
-	Traversal-safe method to add a siblings data node.
-	:param data: The data object you're traversing.
-	:param node_path: List of path segments pointing to the node you're creating a
-			sibling of. Same as node_path of traverse()
-	:param new_key: The sibling key to create.
-	:param new_data: The new data to be stored at the key.
-	"""
-	if _i < len(node_path) - 1:
-		return add_sibling(data[node_path[_i]], node_path, new_key, new_data, _i + 1)
-	else:
-		data[new_key] = new_data
-
-
-
